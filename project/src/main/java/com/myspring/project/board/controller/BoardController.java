cb7f61d845715672b0ab82d5e26416fcc4ea9327
yoosumin123 <tnals4211@naver.com>
2022-09-13 17:53:44 +0900

    새 글 쓰기 ing

--- project/src/main/java/com/myspring/project/board/controller/BoardController.java
+++ project/src/main/java/com/myspring/project/board/controller/BoardController.java
@@ -3,11 +3,13 @@ package com.myspring.project.board.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 public interface BoardController {
 	// 전체 글 목록 조회
 	public ModelAndView listArticles(HttpServletRequest request, HttpServletResponse response) throws Exception;
-	
+	public ResponseEntity addNewArticle(MultipartHttpServletRequest multipartRequest, )
 
 }
